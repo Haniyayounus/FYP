5a87518c6adc84cf540d60467334ef5701423ce8
Haniya Younus <haniya.younus@intelligenes.net>
2021-08-09 17:14:40 +0500

    deployemnt

--- Medius/Program.cs
+++ Medius/Program.cs
@@ -1,11 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Medius
 {
@@ -20,6 +15,8 @@ namespace Medius
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.UseIISIntegration();
                     webBuilder.UseStartup<Startup>();
                 });
     }
