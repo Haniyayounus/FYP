ab62065f8a6cc40799733940b88e3342ed630bca
Haniyayounus <56742335+Haniyayounus@users.noreply.github.com>
2021-09-21 23:31:33 +0500

    make fields nullable

--- Medius.DataAccess/Migrations/20210921182853_MakeClaimIdNullable.Designer.cs
+++ Medius.DataAccess/Migrations/20210921182853_MakeClaimIdNullable.Designer.cs
@@ -4,14 +4,16 @@ using Medius.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Medius.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210921182853_MakeClaimIdNullable")]
+    partial class MakeClaimIdNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
